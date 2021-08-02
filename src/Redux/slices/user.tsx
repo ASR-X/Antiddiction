941b1e99b1b25913b1601cb3a695ae584052dc8d
Neil Song <song.neil.song@gmail.com>
2021-08-02 12:38:16 -0500

    delete auth

--- src/Redux/slices/user.tsx
+++ src/Redux/slices/user.tsx
@@ -12,105 +12,26 @@ import {
 } from 'firebase/auth'
 import * as Linking from 'expo-linking'
 
-export const login = createAsyncThunk(
-  'users/login',
-  async (
-    userData: { email: string; password: string },
-    { rejectWithValue }: any
-  ) => {
-    const { email, password } = userData
-    return signInWithEmailAndPassword(getAuth(Firebase), email, password)
-      .then((response) => {
-        return {
-          email: response.user.email,
-          fullName: response.user.displayName,
-          uid: response.user.uid,
-        }
-      })
-      .catch((err) => {
-        return rejectWithValue(err.code)
-      })
-  }
-)
-
-export const signup = createAsyncThunk(
-  'users/signup',
-  async (
-    userData: { email: string; password: string; fullName: string },
-    { rejectWithValue }: any
-  ) => {
-    const { email, password, fullName } = userData
-    return createUserWithEmailAndPassword(getAuth(Firebase), email, password)
-      .then(async (response) => {
-        await updateProfile(response.user, { displayName: fullName })
-        return {
-          email: response.user.email,
-          fullName: response.user.displayName,
-          uid: response.user.uid,
-        }
-      })
-      .catch((err) => {
-        return rejectWithValue(err.code)
-      })
-  }
-)
-
-export const logout = createAsyncThunk(
-  'users/logout',
-  async (arg, { rejectWithValue }: any) => {
-    return signOut(getAuth(Firebase))
-      .then(() => {
-        return {
-          email: null as string,
-          fullName: null as string,
-          uid: null as string,
-        }
-      })
-      .catch((err) => {
-        return rejectWithValue(err)
-      })
-  }
-)
-
-export const sendReset = createAsyncThunk(
-  'user/sendReset',
-  async (email: string, { rejectWithValue }: any) => {
-    var actionCodeSettings = {
-      url: 'https://food-lines-40c3c.firebaseapp.com/__/auth/action',
-      android: {
-        packageName: 'com.foodlines',
-        installApp: false,
-      },
-      handleCodeInApp: true,
-    }
-    //console.log(actionCodeSettings);
-    return sendPasswordResetEmail(getAuth(Firebase), email, actionCodeSettings)
-      .then(() => {
-        return {
-          email: email,
-          fullName: null,
-          uid: null,
-        }
-      })
-      .catch(function (error) {
-        return rejectWithValue(error)
-      })
-  }
-)
-
-export const resetPass = createAsyncThunk(
-  'user/resetPass',
-  async (
-    data: { code: string; password: string },
-    { rejectWithValue }: any
-  ) => {
-    confirmPasswordReset(getAuth(Firebase), data.code, data.password)
-      .then()
-      .catch(function (error) {
-        return rejectWithValue(error)
-      })
-  }
-)
+// export const login = createAsyncThunk(
+//   'users/login',
+//   async (
+//     userData: { email: string; password: string },
+//     { rejectWithValue }: any
+//   ) => {
+//     const { email, password } = userData
+//     return signInWithEmailAndPassword(getAuth(Firebase), email, password)
+//       .then((response) => {
+//         return {
+//           email: response.user.email,
+//           fullName: response.user.displayName,
+//           uid: response.user.uid,
+//         }
+//       })
+//       .catch((err) => {
+//         return rejectWithValue(err.code)
+//       })
+//   }
+// )
 
 const initialState = {
   user: {
