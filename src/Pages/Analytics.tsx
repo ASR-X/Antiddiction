cb58e72dac057eb95c7da3fd2130195c068742e4
byronli8565 <phoenixashes63@gmail.com>
2021-08-02 21:47:05 -0500

    Add calendar

--- src/Pages/Analytics.tsx
+++ src/Pages/Analytics.tsx
@@ -6,24 +6,10 @@ import { MainRoutes } from '../Navigators/routes'
 import CardView from '../Components/CardView'
 import { BezierGraph } from '../Components/BezierGraph'
 import { ProgressRings } from '../Components/ProgressRings'
-import { ChonseSelect } from 'react-native-chonse-select'
-
+import CalendarHeatmap from 'react-native-calendar-heatmap'
 //Data
 import RecentlyViewed from '../Data/RecentlyViewed'
-const data = [
-  {
-    value: '0',
-    label: '1 M',
-  },
-  {
-    value: '1',
-    label: '3 M',
-  },
-  {
-    value: '2',
-    label: '1 Y',
-  },
-]
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 //Components
 import {
   SafeAreaView,
@@ -66,6 +52,7 @@ const { primary, white, grey, black } = Colors
 
 //Icons
 import { Ionicons, Fontisto } from '@expo/vector-icons'
+import { maxWidth } from 'styled-system'
 
 const Analytics = ({ navigation }): React.ReactElement => {
   const renderItem = ({ item }) => {
@@ -117,6 +104,16 @@ const Analytics = ({ navigation }): React.ReactElement => {
             ],
           }}
         />
+        <CalendarList
+          // Enable horizontal scrolling, default = false
+          horizontal={true}
+          // Enable paging on horizontal, default = false
+          pagingEnabled={true}
+          // Set custom calendarWidth.
+          calendarWidth={Dimensions.get('window').width}
+        />
+                
+                
       </SafeAreaView>
     </View>
   )
