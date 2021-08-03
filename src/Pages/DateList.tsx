cd827b6b4be8973b72bea63b5910bd9ab8b5b185
byronli8565 <phoenixashes63@gmail.com>
2021-08-03 07:41:18 -0500

    faq

---
+++ src/Pages/DateList.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+import { MainRoutes } from '../Navigators/routes'
+
+//Cardview
+import CardView from '../Components/CardView'
+import { BezierGraph } from '../Components/BezierGraph'
+import { ProgressRings } from '../Components/ProgressRings'
+import { ChonseSelect } from 'react-native-chonse-select'
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars'
+
+//Data
+import RecentlyViewed from '../Data/RecentlyViewed'
+const data = [
+  {
+    value: '0',
+    label: '1 M',
+  },
+  {
+    value: '1',
+    label: '3 M',
+  },
+  {
+    value: '2',
+    label: '1 Y',
+  },
+]
+//Components
+import {
+  SafeAreaView,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Image,
+  Dimensions,
+} from 'react-native'
+
+import {
+  Colors,
+  StyledHeader,
+  ProfilePicture,
+  TextWrapper,
+  CategoryContainer,
+  CategoryIcon,
+  CategoryButton,
+  CategoryButtonText,
+  CardImage,
+  CardInfo,
+  CardWrapper,
+  CardImageWrapper,
+  CardTitle,
+  CardDetails,
+} from '../Components/styles'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+} from 'react-native-chart-kit'
+//Slider
+import VerticalSwiper from '../Components/VerticalSwiper'
+
+// Colors
+const { primary, white, grey, black } = Colors
+
+//Icons
+import { Ionicons, Fontisto } from '@expo/vector-icons'
+
+const DateList = ({ navigation }): React.ReactElement => {
+
+  return (
+    <View style={{ flex: 1, backgroundColor: white }}>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: white,
+          alignContent: 'center',
+          marginTop: 10,
+        }}
+      >
+        <CalendarList
+          // Enable horizontal scrolling, default = false
+          horizontal={true}
+          // Enable paging on horizontal, default = false
+          pagingEnabled={true}
+          // Set custom calendarWidth.
+          calendarWidth={Dimensions.get('window').width}
+        />
+        <CalendarList
+          // Enable horizontal scrolling, default = false
+          horizontal={true}
+          // Enable paging on horizontal, default = false
+          pagingEnabled={true}
+          // Set custom calendarWidth.
+          calendarWidth={Dimensions.get('window').width}
+        />
+      </SafeAreaView>
+    </View>
+  )
+}
+
+export default DateList
