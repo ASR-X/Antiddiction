a98821daaa0aa2f2d07167fe73aa13bceb1e6b9e
Lint Action <lint-action@vmware-dev.com>
2021-09-18 23:46:53 +0000

    Fix code style issues with Prettier

--- src/Pages/Question1.tsx
+++ src/Pages/Question1.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { StatusBar, View, StyleSheet, TextInput, TouchableWithoutFeedback } from 'react-native'
+import {
+  StatusBar,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+} from 'react-native'
 
 import { MainRoutes } from '../Navigators/routes'
 import {
@@ -21,7 +27,7 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { justifyContent } from 'styled-system'
 import { useReduxDispatch, useReduxSelector } from '../Redux'
 import userSlice, { selectUser } from '../Redux/slices/user'
-import {Keyboard} from 'react-native'
+import { Keyboard } from 'react-native'
 
 const { primary, white, grey, black } = Colors
 
@@ -32,44 +38,44 @@ export const Question1 = ({ navigation }): React.ReactElement => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View style={{ flex: 1, backgroundColor: white }}>
-      <StatusBar barStyle="light-content" />
-      <QuestionHeaderView />
-      <Animatable.View style={styles.footer}>
-        <View style={{ flex: 1 }}>
-          <QuestionTitle>Age</QuestionTitle>
-          <TextInput
-            selectionColor={white}
-            style={styles.textInput}
-            onChangeText={onChangeAge}
-            placeholder="18"
-            keyboardType="numeric"
-          />
-        </View>
-        <View style={{ flex: 1 }}>
-          <QuestionTitle>Age at First Use</QuestionTitle>
-          <TextInput
-            selectionColor={white}
-            style={styles.textInput}
-            onChangeText={onChangeFirstAge}
-            placeholder="16"
-            keyboardType="numeric"
-          />
-        </View>
-        <QuestionNextButton
-          onPress={() => {
-            if (age && firstAge && parseInt(firstAge) <= parseInt(age)) {
-              dispatch(
-                userSlice.actions.setFactor({ age: age, firstAge: firstAge })
-              )
-              navigation.navigate(MainRoutes.Question2)
-            }
-          }}
-        >
-          <MaterialIcons name="navigate-next" color={white} size={45} />
-        </QuestionNextButton>
-      </Animatable.View>
-    </View>
+      <View style={{ flex: 1, backgroundColor: white }}>
+        <StatusBar barStyle="light-content" />
+        <QuestionHeaderView />
+        <Animatable.View style={styles.footer}>
+          <View style={{ flex: 1 }}>
+            <QuestionTitle>Age</QuestionTitle>
+            <TextInput
+              selectionColor={white}
+              style={styles.textInput}
+              onChangeText={onChangeAge}
+              placeholder="18"
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            <QuestionTitle>Age at First Use</QuestionTitle>
+            <TextInput
+              selectionColor={white}
+              style={styles.textInput}
+              onChangeText={onChangeFirstAge}
+              placeholder="16"
+              keyboardType="numeric"
+            />
+          </View>
+          <QuestionNextButton
+            onPress={() => {
+              if (age && firstAge && parseInt(firstAge) <= parseInt(age)) {
+                dispatch(
+                  userSlice.actions.setFactor({ age: age, firstAge: firstAge })
+                )
+                navigation.navigate(MainRoutes.Question2)
+              }
+            }}
+          >
+            <MaterialIcons name="navigate-next" color={white} size={45} />
+          </QuestionNextButton>
+        </Animatable.View>
+      </View>
     </TouchableWithoutFeedback>
   )
 }
